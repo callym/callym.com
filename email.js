8714a1c61d4af3e678dfd65a6bd94c1227ae83f5
Callym <hi@callym.com>
2016-09-28 12:19:44 +0100

    changed email.js to be more modular

--- email.js
+++ email.js
@@ -1,113 +1,125 @@
 var fs = require('fs');
 var path = require('path');
-var EmailTemplate = require('email-templates').EmailTemplate;
-var nodemailer = require('nodemailer');
-var async = require('async');
 
 email_auth = JSON.parse(fs.readFileSync('./email/newsletter_auth.json'));
 s3_auth = JSON.parse(fs.readFileSync('./email/s3_auth.json'));
 
-email_data = JSON.parse(fs.readFileSync('./email/emails/' + process.argv[2]));
-var data_path = path.parse(process.argv[2]);
-var assets_path = `assets/${data_path.dir}/${data_path.name}/`;
+exports.send_dry_email = function send_dry_email(email) { exports.send_email(email, true); };
 
-s3_sync();
+exports.send_email = function send_email(email, dry_run = false) {
+	exports.sync_assets();
 
-var templatesDir = path.resolve(__dirname, './', 'email', 'templates');
-var template = new EmailTemplate(path.join(templatesDir, email_data.template), {
-	sassOptions: {
-		includePaths: ['../callym.com/sass']
+	email_data = JSON.parse(fs.readFileSync('./email/emails/' + email));
+	var data_path = path.parse(email);
+	var assets_path = `${data_path.dir}/${data_path.name}/`;
+
+	var EmailTemplate = require('email-templates').EmailTemplate;
+	var nodemailer = require('nodemailer');
+	var async = require('async');
+
+	var transport = nodemailer.createTransport({
+		service: 'SES-EU-WEST-1',
+		auth: {
+			user: email_auth.user,
+			pass: email_auth.pass
+		}
+	});
+
+	var s3_prefix = `https://s3-eu-west-1.amazonaws.com/${s3_auth.bucket}/assets/`;
+	var constants = {
+		s3_prefix: s3_prefix,
+		assets_path: s3_prefix + assets_path,
+		base_url: "https://beta.callym.com"
 	}
-});
+	var users = [];
 
-var transport = nodemailer.createTransport({
-	service: 'SES-EU-WEST-1',
-	auth: {
-		user: email_auth.user,
-		pass: email_auth.pass
-	}
-});
-
-var email = {
-	preheader: email_data.description,
-	topic: email_data.template,
-}
-
-var s3_prefix = `https://s3-eu-west-1.amazonaws.com/${s3_auth.bucket}/assets/`;
-var constants = {
-	s3_prefix: s3_prefix,
-	assets_path: s3_prefix + assets_path,
-	base_url: "https://beta.callym.com"
-}
-
-var users = [];
-
-var aws = require('aws-sdk');
-aws.config.update({
-	region: 'eu-west-1'
-});
-var doc_client = new aws.DynamoDB.DocumentClient();
-var params = {
-	TableName: 'callym-com-email'
-};
-doc_client.scan(params, on_scan);
-function on_scan(error, data) {
-	if (error) {
-		console.log("ERROR: ", JSON.stringify(error));
-	}
-	else {
-		data.Items.forEach(function(item, i, arr) {
-			if (item.confirmed) {
-				if (email.topic == 'all' || item.topics.indexOf(email.topic) > -1) {
-					users.push(item);
+	var aws = require('aws-sdk');
+	aws.config.update({
+		region: 'eu-west-1'
+	});
+	var doc_client = new aws.DynamoDB.DocumentClient();
+	var params = {
+		TableName: 'callym-com-email'
+	};
+	doc_client.scan(params, on_scan);
+	function on_scan(error, data) {
+		if (error) {
+			console.log("ERROR: ", JSON.stringify(error));
+		}
+		else {
+			data.Items.forEach(function(item, i, arr) {
+				if (item.confirmed) {
+					if (email.topic == 'all' || item.topics.indexOf(email.topic) > -1) {
+						users.push(item);
+					}
 				}
+			});
+
+			if (typeof data.LastEvaluatedKey != "undefined") {
+				params.ExclusiveStartKey = data.LastEvaluatedKey;
+				doc_client.scan(params, on_scan);
+			} else {
+				send_emails();
+			}
+		}
+	};
+
+	var templatesDir = path.resolve(__dirname, './', 'email', 'templates');
+	var template = new EmailTemplate(path.join(templatesDir, email_data.template), {
+		sassOptions: {
+			includePaths: ['./sass']
+		}
+	});
+
+	var email = {
+		preheader: email_data.description,
+		topic: email_data.template,
+	}
+
+	dry_run = dry_run || false;
+	
+	function send_emails() {
+		async.mapLimit(users, 10, function (item, next) {
+			var item = Object.assign(constants, email, email_data, item);
+
+			if (dry_run) {
+				console.log(item);
+			}
+
+			template.render(item, function (err, results) {
+				if (err) return next(err)
+				if (!dry_run) {
+					transport.sendMail({
+						from: 'Callym <newsletter@callym.com>',
+						to: item.email,
+						subject: item.title,
+						html: results.html
+					}, function (err, responseStatus) {
+						if (err) {
+							return next(err)
+						}
+						console.log('sent to: ' + item.email);
+						next(null, responseStatus.message)
+					});
+				} else {
+					console.log('would have sent to: ' + item.email);
+					next(null);
+				}
+			})
+		}, function (err) {
+			if (err) {
+				console.error(err)
+			}
+			if (dry_run) {
+				console.log(`would have sent ${users.length} emails`);
+			} else {
+				console.log(`succesfully sent ${users.length} messages`);
 			}
 		});
-
-		if (typeof data.LastEvaluatedKey != "undefined") {
-			params.ExclusiveStartKey = data.LastEvaluatedKey;
-			doc_client.scan(params, on_scan);
-		} else {
-			send_emails();
-		}
-	}
+	};
 };
 
-var dry_run = true;
-function send_emails() {
-	async.mapLimit(users, 10, function (item, next) {
-		var item = Object.assign(constants, email, email_data, item);
-		console.log(item);
-
-		template.render(item, function (err, results) {
-			if (err) return next(err)
-			if (!dry_run) {
-				transport.sendMail({
-					from: 'Callym <newsletter@callym.com>',
-					to: item.email,
-					subject: item.title,
-					html: results.html
-				}, function (err, responseStatus) {
-					if (err) {
-						return next(err)
-					}
-					console.log('sent to: ' + item.email);
-					next(null, responseStatus.message)
-				});
-			} else {
-				console.log('would have sent to: ' + item.email);
-				next(null);
-			}
-		})
-	}, function (err) {
-		if (err) {
-			console.error(err)
-		}
-		console.log('Succesfully sent %d messages', users.length)
-	});
-};
-
-function s3_sync() {
+exports.sync_assets = function sync_assets() {
 	var s3 = require('s3');
 	var s3_client = s3.createClient({
 		s3Options: {
@@ -129,3 +141,5 @@ function s3_sync() {
 		console.log("done uploading");
 	});
 }
+
+require('make-runnable');
